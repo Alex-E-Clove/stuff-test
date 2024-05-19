de174fa103dc6abc7d05cc149317df58a0f84ee0
Alexander Eklöw <eklow.alexander@gmail.com>
2024-05-19 19:26:21 +0200

    remove edge experimental

--- pages/[...slug].ts
+++ pages/[...slug].ts
@@ -1,50 +1,53 @@
-import { client } from '../tina/__generated__/client';
-import { getNavItems } from '../lib/nav';
-import PageComponent from '../components/PageComponent';
-import { GetStaticProps, GetStaticPaths } from 'next';
+import { client } from "../tina/__generated__/client";
+import { getNavItems } from "../lib/nav";
+import PageComponent from "../components/PageComponent";
+import { GetStaticProps, GetStaticPaths } from "next";
 export default PageComponent;
-export const runtime = 'experimental-edge'; // 'nodejs' (default) | 'edge'
+export const runtime = "edge"; // 'nodejs' (default) | 'edge'
 
 export const getStaticPaths: GetStaticPaths = async () => {
-    const { data } = await client.queries.pageConnection();
-    const edges = data.pageConnection.edges ?? [];
-    const paths = edges.flatMap((post) => {
-        return post && post.node
-            ? [
-                  { params: { slug: post.node._sys.breadcrumbs }, locale: 'en' },
-                  { params: { slug: post.node._sys.breadcrumbs }, locale: 'sv' },
-              ]
-            : [];
-    });
-    return {
-        paths,
-        fallback: 'blocking',
-    };
+  const { data } = await client.queries.pageConnection();
+  const edges = data.pageConnection.edges ?? [];
+  const paths = edges.flatMap((post) => {
+    return post && post.node
+      ? [
+          { params: { slug: post.node._sys.breadcrumbs }, locale: "en" },
+          { params: { slug: post.node._sys.breadcrumbs }, locale: "sv" },
+        ]
+      : [];
+  });
+  return {
+    paths,
+    fallback: "blocking",
+  };
 };
 
 // This is an example of a page generated with Serverside Rendering.
 // This can be switched to a static page by using getStaticProps
-export const getStaticProps: GetStaticProps = async ({ params, preview = false }) => {
-    const slug = (params?.slug as String[]) ?? [];
-    const { data, query, variables } = await client.queries.page({
-        relativePath: `/${slug.join('/')}.mdx`,
-    });
-    let navItems = await getNavItems({ preview });
-    console.log('navItems', navItems);
-    const footerProps = await client.queries.footer({
-        relativePath: 'footer.json',
-    });
-    const navbarProps = await client.queries.navbar({
-        relativePath: 'navbar.json',
-    });
-    return {
-        props: {
-            data,
-            query,
-            variables,
-            navItems,
-            footerProps,
-            navbarProps,
-        },
-    };
+export const getStaticProps: GetStaticProps = async ({
+  params,
+  preview = false,
+}) => {
+  const slug = (params?.slug as String[]) ?? [];
+  const { data, query, variables } = await client.queries.page({
+    relativePath: `/${slug.join("/")}.mdx`,
+  });
+  let navItems = await getNavItems({ preview });
+  console.log("navItems", navItems);
+  const footerProps = await client.queries.footer({
+    relativePath: "footer.json",
+  });
+  const navbarProps = await client.queries.navbar({
+    relativePath: "navbar.json",
+  });
+  return {
+    props: {
+      data,
+      query,
+      variables,
+      navItems,
+      footerProps,
+      navbarProps,
+    },
+  };
 };
